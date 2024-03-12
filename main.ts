7b0c1079906c6825a5b0e365620caa965927fdab
Alamzaibsahito <162956522+Alamzaibsahito@users.noreply.github.com>
2024-03-12 14:56:31 +0500

    4th assignment
    Arithmetic operators

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
-let favNumber = 10;
-let message = "mine favorite number is:"
-console.log( message,favNumber);
+let n1 = 50;
+let n2 = 20;
+console.log(n1 + n2); //Subtraction
 
+let n3 = 5;
+let n4 = 5;
+console.log(n3 * n4); //Multipication
 
-let favoriteColor = "yellow,and,green";
-console.log (favoriteColor);
+let n5 = 4;
+let n6 = 2;
+console.log(n5 / n6); //Division
+
+let n7 = 4;
+let n8 = 4;
+console.log(n7 ** n8); //Exponentiation
+
+let n9 = 20;
+let n10 = 3;
+console.log(n9 % n10); //Modulus
