e34a8b4da72af2a382be86cde0e7276796d388e6
Alamzaibsahito <162956522+Alamzaibsahito@users.noreply.github.com>
2024-03-16 10:42:24 +0500

    6th assignement
    for loop

--- main.ts
+++ main.ts
@@ -1,13 +1,36 @@
-// //array
-let fruits =["apple","banana","lemon"]
-console.log(fruits.pop())
-console.log(fruits .push("kiwi","grapes"))
-console.log(fruits)
- console.log(fruits.shift())
-console.log(fruits)
-console.log(fruits.unshift("pineapple"))
-console.log(fruits)
-console.log(fruits.splice(3,0,"grapes","cake","kiwi"))
-console.log(fruits)
-console.log(fruits.slice(2))
-console.log(fruits)
+let one = "1"
+let two = "2"
+let three = "3"
+let four = "4"
+let five = "5"
+
+console.log(one,two,three,four,five)
+
+
+
+//for loop
+
+loop format /syntax
+ for (let h =1;h<=4 h++){
+    console.log("hello world")
+}
+
+
+
+let cleanestCities : = [ "Matli","Tando Ghulam Ali","New Dumbalo"];
+ for (let i =1; 1<=4; i++){
+    console.log("it is one of the cleanest cities");
+    break;
+ }
+
+let n:number =4;
+let a:string="";
+for (let i:number=1; i<=n; i++){
+    //console.log(i)
+    for (let j:number=1; j<=i;j++){
+        a +="*"
+        
+    }
+    a += "\n"
+}
+console.log(a)
