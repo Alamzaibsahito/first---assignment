8231ca186c8b9233465c8da8ca8617b976bc5a94
Alamzaibsahito <162956522+Alamzaibsahito@users.noreply.github.com>
2024-03-20 14:38:14 +0500

    18
    Assignment

--- main.ts
+++ main.ts
@@ -1,4 +1,22 @@
-const num1 = 23;
-const num2 = 3;
-//this will add the num1  and num2 (this comment is typescript)
-console.log(num1 + num2);
+
+
+const myFriends: string []= ["Khan","Zeeshan","bilal","Azhar", "Rehaman",]
+console.log(`table is not available I can invite only 2 person\n`);
+
+let  friend1= myFriends.pop();
+console.log(`Sorry Mr. ${friend1} you are not invited!`);
+
+let  friend2= myFriends.pop();
+console.log(`Sorry Mr. ${friend2} you are not invited!`);
+
+let  friend3= myFriends.pop();
+console.log(`Sorry Mr. ${friend3} you are not invited!\n`);
+
+for (let i = 0;i <myFriends.length;i++){
+    console.log(`Mr. ${myFriends[i]} you are still inivited`)
+}
+
+myFriends.pop();
+myFriends.pop();
+
+console.log(myFriends);
